6e37f9a8e173c33ecd3ef5ea09dcb67acacd173a
getneeditofficial-art <getneeditofficial@gmail.com>
2026-02-25 23:43:40 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const Razorpay = require("razorpay");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Razorpay keys (Dashboard se lo)
+const razorpay = new Razorpay({
+  key_id: "rzp_test_SKT6icJlDTrjXI",
+  key_secret: "CnmKhvVtuDjLT8DgvocDNF9L"
+});
+
+// API to create order
+app.post("/createOrder", async (req, res) => {
+  const amount = req.body.amount;
+
+  const options = {
+    amount: amount * 100, // rupees to paise
+    currency: "INR",
+    receipt: "receipt1"
+  };
+
+  try {
+    const order = await razorpay.orders.create(options);
+    res.json(order);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Order creation failed");
+  }
+});
+
+// start server
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
